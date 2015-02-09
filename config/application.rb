3963157a95298820ab06c3bf30d298e2d25d42f3
Ben Radler <benradler@me.com>
2015-02-08 21:36:53 -0800

    Load config/local_env.yml via application.rb

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,17 @@ module Brog
     # add paths to asset pipeline
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+    # load environment variables from local_env.yml
+    def config_local_env
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file))[Rails.env].each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
+    config.before_configuration do
+      config_local_env
+    end
   end
 end
