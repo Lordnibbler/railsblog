a26dbddf8032dbc963c4abd4e4ab1aed0b50c9b6
Ben Radler <benradler@me.com>
2015-02-09 00:11:44 -0800

    honeypot captcha working

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -1,14 +1,10 @@
 class ContactFormsController < ApplicationController
   def create
-    contact_form = ContactForm.new(params[:contact_form])
-    contact_form.request = request # append remote ip, user agent and session
-
-    if contact_form.deliver
+    if ContactForm.new(params[:contact_form].merge(request: request)).deliver
       flash[:success] = 'Email sent successfully'
     else
       flash[:error] = 'Email failed to send'
     end
-
     redirect_to page_path(:contact_me)
   end
 end
