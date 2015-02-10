d7f49001ac3c5b5e053ce8acd31c9f509be6c52c
Ben Radler <benradler@me.com>
2015-02-09 22:10:46 -0800

    Test configuration, post tests, update readme

---
+++ spec/models/blog/post_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe Blog::Post, type: :model do
+  fixtures :users, :posts
+
+  let(:post)      { posts(:alpha) }
+  let(:long_post) { posts(:beta) }
+
+  describe 'author' do
+    let(:user) { users(:ben) }
+
+    context 'when no name' do
+      before { user.update_attribute(:name, '') }
+      it 'returns a thing' do
+        expect(post.author).to eql('')
+      end
+    end
+
+    context 'when name' do
+      it 'returns a human-readable name' do
+        expect(post.author).to eql('Ben Radler')
+      end
+    end
+  end
+
+  describe 'excerpt' do
+    context 'when no EXCERPT_TAG' do
+      it 'returns all of the body' do
+        expect(post.excerpt).to eql(post.body)
+      end
+    end
+
+    context 'when EXCERPT_TAG' do
+      it 'returns only the body content before the EXCERPT_TAG' do
+        expect(long_post.excerpt).to_not eql(long_post.body)
+        expect(long_post.excerpt).to_not include('<!--more-->')
+      end
+    end
+  end
+
+  describe 'has_more_text?' do
+    context 'when no EXCERPT_TAG' do
+      it 'returns false' do
+        expect(post.has_more_text?).to be false
+      end
+    end
+
+    context 'when EXCERPT_TAG' do
+      it 'returns true' do
+        expect(long_post.has_more_text?).to be true
+      end
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,4 +47,9 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.before(:all) do
+    # custom fixtures classes
+    self.class.set_fixture_class posts: Blog::Post
+  end
 end
