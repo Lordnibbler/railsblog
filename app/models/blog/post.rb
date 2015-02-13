a933d0c4e08baf789fb751ec50e8c996b84b72b9
Ben Radler <benradler@me.com>
2015-02-12 22:48:43 -0800

    Add validators to Blog::Post model

--- app/models/blog/post.rb
+++ app/models/blog/post.rb
@@ -4,6 +4,8 @@ class Blog::Post < ActiveRecord::Base
 
   belongs_to :user
   validates :user_id, presence: true
+  validates :title,   presence: true, uniqueness: true
+  validates :body,    presence: true
 
   scope :published, -> { where(published: true) }
   scope :newest,    -> { order('created_at DESC') }
