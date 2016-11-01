750b284ded6c54d7307592ea6dd3a607228d771d
Ben Radler <benradler@me.com>
2016-10-31 23:10:59 -0700

    Clean up contact_forms_controller

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -3,8 +3,21 @@
 #
 class ContactFormsController < ApplicationController
   def create
-    sent = ContactForm.new(params[:contact_form].merge(request: request)).deliver
-    sent ? flash[:success] = 'Email sent successfully' : flash[:error] = 'Email failed to send'
+    if deliver_contact_form!
+      flash[:success] = 'Email sent successfully'
+    else
+      flash[:error] = 'Email failed to send'
+    end
     redirect_to page_path('contact-me')
+  end
+
+  private
+
+  def deliver_contact_form!
+    contact_form.deliver
+  end
+
+  def contact_form
+    @contact_form ||= ContactForm.new(params[:contact_form].merge(request: request))
   end
 end
