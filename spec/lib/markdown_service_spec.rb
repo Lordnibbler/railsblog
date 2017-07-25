6f05f9cf05b0919c3662fb77614d07b295bf1b71
Ben Radler <ben@benradler.com>
2017-07-24 23:14:16 -0700

    Upgade to rails 5.1.2 and ruby 2.4.1
    Upgrade ruby in travis
    
    wip
    
    remove

--- spec/lib/markdown_service_spec.rb
+++ spec/lib/markdown_service_spec.rb
@@ -25,11 +25,11 @@ describe MarkdownService do
       end
 
       let(:fenced_html) do
-        "<pre class=\"highlight ruby\"><code><span class=\"c1\"># this is some ruby code</span>\n" \
+        "<div class=\"highlight\"><pre class=\"highlight ruby\"><code><span class=\"c1\"># this is some ruby code</span>\n" \
           '<span class="k">def</span> <span class="nf">foo</span><span class="p">(</span>'   \
           "<span class=\"n\">bar</span> <span class=\"o\">=</span> <span class=\"s1\">'baz'"       \
           "</span><span class=\"p\">)</span>\n  <span class=\"nb\">puts</span> <span class=\"n\">" \
-          "bar</span>\n<span class=\"k\">end</span>\n</code></pre>"
+          "bar</span>\n<span class=\"k\">end</span>\n</code></pre></div>"
       end
 
       it 'returns formatted <code>' do
