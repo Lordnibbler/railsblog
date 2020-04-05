1f844c82207956a44de28b2969699f12f54ec200
Ben Radler <ben@benradler.com>
2020-04-05 01:36:56 -0700

    Migrate capybara from phantomjs to selenium

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 require 'capybara/rails'
-require 'capybara/poltergeist'
 
 # stub logged in devise user w/ capybara
 # @see https://github.com/plataformatec/devise/wiki/How-To:-Test-with-Capybara
 include Warden::Test::Helpers
 Warden.test_mode!
 
-# use phantomjs headless driver
-Capybara.javascript_driver = :poltergeist
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: %w[headless]
+    }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+# set to :chrome to see real browser open up
+Capybara.default_driver = :headless_chrome
+Capybara.javascript_driver = :headless_chrome
+
 Capybara.server = :webrick
