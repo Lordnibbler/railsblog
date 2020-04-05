388d4ccbc31a4e5c1e0b0b50477b9a70e6969d05
Ben Radler <ben@benradler.com>
2020-04-05 02:03:26 -0700

    improve test

--- spec/features/contact_form_spec.rb
+++ spec/features/contact_form_spec.rb
@@ -5,14 +5,14 @@ describe '/contact-me', js: true do
 
   context 'with invalid data' do
     it 'shows inline validations' do
+      click_on 'Send'
+      expect(page).to have_selector('label[for=contact_form_name]', text: 'can\'t be blank')
+
       within '#new_contact_form' do
         fill_in 'contact_form_email', with: 'not an email'
       end
       click_on 'Send'
-
-      expect(page).to have_selector('label[for=contact_form_name]', text: 'can\'t be blank')
       expect(page).to have_selector('label[for=contact_form_email]', text: 'is invalid')
-      expect(page).to have_selector('label[for=contact_form_name]', text: 'can\'t be blank')
     end
   end
 
