50b8c401ac0e595c7a3dda012b5499228c0fa264
Ben Radler <ben@benradler.com>
2020-04-26 14:38:46 -0700

    Add photography_controller_spec

---
+++ spec/controllers/photography_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe PhotographyController do
+  let(:photo) do
+    { source: 'flickr', key: '14224430131' }
+  end
+
+  describe 'index' do
+    it 'fetches photos for the passed page param' do
+      allow(FlickrService).to receive(:get_photos).and_return([photo])
+
+      get :index, params: { page: 1 }
+
+      expect(assigns(:photos)).to eq([photo])
+    end
+
+    it 'returns empty list when get_photos returns nil' do
+      allow(FlickrService).to receive(:get_photos).and_return(nil)
+
+      get :index, params: { page: 100 }
+
+      expect(assigns(:photos)).to eq([])
+    end
+  end
+end
