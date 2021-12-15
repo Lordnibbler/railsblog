7456c160748f7b6ffab7998803d1a09185aaf388
Ben Radler <ben@benradler.com>
2021-12-14 22:47:22 -0500

    reorder postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    require("tailwindcss")('./tailwind.config.js'),
-    require('autoprefixer'),
     require('postcss-import'),
+    require('autoprefixer'),
+    require("tailwindcss"),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
