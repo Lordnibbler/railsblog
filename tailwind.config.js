8f67cf5f8680e5bb062efa7d3af6b6a4d3f26980
Ben Radler <ben@benradler.com>
2021-12-14 22:49:11 -0500

    beautify

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     purge: [
-      './app/**/*.html.erb',
-      './app/**/*.html.haml',
-      './app/helpers/**/*.rb',
-      './app/javascript/**/*.js',
-      './app/javascript/**/*.vue',
-      './app/javascript/**/*.jsx'
+        './app/**/*.html.erb',
+        './app/**/*.html.haml',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+        './app/javascript/**/*.jsx'
     ],
     darkMode: "media",
     theme: {
@@ -16,8 +16,8 @@ module.exports = {
             body: ['Open Sans', "sans-serif"]
         },
         screens: {
-          xs: "375px",
-          ...defaultTheme.screens
+            xs: "375px",
+            ...defaultTheme.screens
         },
         colors: {
             transparent: "transparent",
@@ -39,61 +39,61 @@ module.exports = {
             "blog-gradient-from": "#8f9098",
             "blog-gradient-to": "#222222"
         },
-      container: {
-          center: true,
-          padding: "1rem"
-      },
-      shadows: {
-          default: '0 2px 18px rgba(0, 0, 0, 0.06)',
-          md: '0 -3px 36px rgba(0, 0, 0, 0.12)',
-      },
-      extend: {
-          spacing: {
-              "13": '3.25rem',
-              "15": '3.75rem',
-              "17": '4.25rem',
-              "18": '4.5rem',
-              "19": '4.75rem',
-              "42": '10.5rem',
-              "76": "19rem",
-              "84": "21rem",
-              "88": "22rem",
-              "92": "23rem",
-              "100": "25rem",
-              "104": "26rem",
-              "108": "27rem",
-              "112": "28rem",
-              "116": "29rem",
-              "120": "30rem",
-              "124": "31rem",
-              "128": "32rem",
-              "132": "33rem",
-              "136": "34rem",
-              "140": "35rem",
-              "144": "36rem",
-              "148": "37rem",
-              "152": "38rem",
-              "156": "39rem",
-              "160": "40rem",
-              "164": "41rem",
-              "168": "42rem",
-              "172": "43rem",
-              "176": "44rem",
-              "180": "45rem",
-              "184": "46rem",
-              "188": "47rem",
-              "190": "48rem",
-              "194": "49rem",
-              "200": "50rem",
-              "204": "51rem"
-          },
-          zIndex: {
-              "-1": "-1"
-          },
-          inset: {
-              '2/5': '40%'
-          }
-      }
+        container: {
+            center: true,
+            padding: "1rem"
+        },
+        shadows: {
+            default: '0 2px 18px rgba(0, 0, 0, 0.06)',
+            md: '0 -3px 36px rgba(0, 0, 0, 0.12)',
+        },
+        extend: {
+            spacing: {
+                "13": '3.25rem',
+                "15": '3.75rem',
+                "17": '4.25rem',
+                "18": '4.5rem',
+                "19": '4.75rem',
+                "42": '10.5rem',
+                "76": "19rem",
+                "84": "21rem",
+                "88": "22rem",
+                "92": "23rem",
+                "100": "25rem",
+                "104": "26rem",
+                "108": "27rem",
+                "112": "28rem",
+                "116": "29rem",
+                "120": "30rem",
+                "124": "31rem",
+                "128": "32rem",
+                "132": "33rem",
+                "136": "34rem",
+                "140": "35rem",
+                "144": "36rem",
+                "148": "37rem",
+                "152": "38rem",
+                "156": "39rem",
+                "160": "40rem",
+                "164": "41rem",
+                "168": "42rem",
+                "172": "43rem",
+                "176": "44rem",
+                "180": "45rem",
+                "184": "46rem",
+                "188": "47rem",
+                "190": "48rem",
+                "194": "49rem",
+                "200": "50rem",
+                "204": "51rem"
+            },
+            zIndex: {
+                "-1": "-1"
+            },
+            inset: {
+                '2/5': '40%'
+            }
+        }
     },
     variants: {
         extend: {
@@ -101,9 +101,8 @@ module.exports = {
         }
     },
     plugins: [
-      require("@tailwindcss/typography"),
-      require("@tailwindcss/forms"),
-      require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
     ]
-  }
-
+}
