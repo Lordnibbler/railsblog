b30355474471e55ddad0d5b987bc334f9dd53dd8
Ben Radler <ben@benradler.com>
2021-12-16 23:53:11 -0500

    no longer using sprockets for images

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,3 @@
-//= link_tree ../images
-
 // NOTE: we used to use sprockets for JS and CSS. As of rails 6, webpack is used for JS by default.
-//       we have also opted to use webpack for CSS for simplicity sake, so we removed the link_directory
+//       we have also opted to use webpack for CSS for simplicity sake, so we removed the link_directory 
 //       directives for javascripts and stylesheets in /app/assets
