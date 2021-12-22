6c4e0150c2ece000715deffb5e9221ae6af75c42
Ben Radler <ben@benradler.com>
2021-12-21 22:26:43 -0500

    Add unitt est for newsletter_signup model

--- spec/models/newsletter_signup_spec.rb
+++ spec/models/newsletter_signup_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe NewsletterSignup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'does not allow invalid emails' do
+    model = NewsletterSignup.new(email: "foo")
+    
+    model.validate
+
+    expect(model.errors[:email]).to eq(['is invalid'])
+  end
+
+  it 'allows valid emails' do
+    model = NewsletterSignup.new(email: "foo@bar.com")
+
+    model.validate
+
+    expect(model.errors[:email]).to eq([])
+  end
 end
