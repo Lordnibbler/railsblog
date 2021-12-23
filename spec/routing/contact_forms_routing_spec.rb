b6b6dd3e80a677e2d945e3e121ecd8e763548ff7
Ben Radler <ben@benradler.com>
2021-12-22 23:07:03 -0500

    add contact forms routing spec

---
+++ spec/routing/contact_forms_routing_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Contact Form' do
+    it 'successfully renders the index template on GET /contact-me', type: :request do
+      get '/contact-me'
+      
+      expect(response).to be_successful
+      expect(response).to render_template('pages/contact-me')
+    end
+
+    it 'successfully renders the index template on GET /pages/contact-me', type: :request do
+        get '/pages/contact-me'
+        
+        expect(response).to be_successful
+        expect(response).to render_template('pages/contact-me')
+      end
+  end
