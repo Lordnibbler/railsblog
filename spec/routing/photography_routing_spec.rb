70f635ebf85bcee48dbeb92dfeba24a66aecad70
Ben Radler <ben@benradler.com>
2021-12-22 23:05:07 -0500

    add photography routing spec

---
+++ spec/routing/photography_routing_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe PhotographyController do
+    it 'successfully renders the index template on GET photography_path', type: :request do
+      get photography_path
+      
+      expect(response).to be_successful
+      expect(response).to render_template(:index)
+    end
+  end
