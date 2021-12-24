7efe361ffed59dc9fc7d5560715244dcddac2cd3
Ben Radler <ben@benradler.com>
2021-12-24 12:57:20 -0500

    minimally working fixed nav on homepage

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -8,4 +8,22 @@ $(document).on('turbo:load', function() {
   $('.flash').on('click', function(event) {
     $(this).slideUp();
   });
+
+  function changeCss () {
+
+    var bodyElement = document.querySelector("body");
+    var navElement = document.querySelector(".desktop-nav");
+    if (this.scrollY > 500) {
+      console.log("changeCss - going NOT transparent")
+      navElement.classList.remove("bg-primary/0")
+      navElement.classList.add("bg-primary/90")
+    } else {
+      console.log("changeCss - going transparent")
+      navElement.classList.remove("bg-primary/90")
+      navElement.classList.add("bg-primary/0")
+    }
+    // this.scrollY > 500 ? navElement.style.opacity = 0.0 : navElement.style.opacity = 1;
+  }
+
+  window.addEventListener("scroll", changeCss , false);
 });
