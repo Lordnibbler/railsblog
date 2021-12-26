2af88c6a9f1946e4722714c07554ae671fd0edcc
Ben Radler <ben@benradler.com>
2021-12-25 23:22:13 -0500

    improve teset

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 describe HomeController do
   let!(:post) { create(:post) }
   let!(:long_post) { create(:long_post, user: post.user) }
+  let!(:unpublished_post) { create(:unpublished_post, user: post.user) }
 
   describe 'get #index' do
     subject(:get_index) { get 'index' }
 
     before { get_index }
 
-    it 'fetches blog posts' do
+    it 'fetches published blog posts' do
       expect(assigns(:posts).count).to eql(2)
       expect(assigns(:posts)).to include(post)
       expect(assigns(:posts)).to include(long_post)
