d35211c8c2fda02553bb3530bd30fbbe376f9efd
Ben Radler <ben@benradler.com>
2021-12-27 23:17:15 -0500

    full and 300px images

--- app/admin/blog_post.rb
+++ app/admin/blog_post.rb
@@ -29,14 +29,22 @@ ActiveAdmin.register Blog::Post do
       row :featured_image_url
       row :featured_image do |post|
         if post.featured_image.attached?
-          link_to(image_tag(url_for(post.featured_image), width: 300), url_for(post.featured_image))
+          # full size original
+          # link_to(image_tag(url_for(post.featured_image), width: 300), url_for(post.featured_image))
+
+          # resized to 300 width max
+          link_to(image_tag(url_for(post.featured_image.representation(resize_to_limit: [300, nil]).processed)), url_for(post))
         end
       end
       row :images do |post|
         div do
           post.images.each do |img|
             span do
-              link_to(image_tag(url_for(img), width: 200), url_for(img))
+              # full size original
+              # link_to(image_tag(url_for(img), width: 200), url_for(img))
+
+              # resized to 300 width max
+              link_to(image_tag(url_for(img.representation(resize_to_limit: [300, nil]).processed)), url_for(img))
             end
             span do
               a "Delete", href: delete_image_admin_blog_post_path(img.id), "data-method": :delete, "data-confirm": "Are you sure?"
