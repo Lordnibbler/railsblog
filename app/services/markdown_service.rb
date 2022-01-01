860cb4114899036709c0314408f4bb711fc8e8f5
Ben Radler <ben@benradler.com>
2022-01-01 14:36:31 -0500

    WIP on blog posts #show and #index

--- app/services/markdown_service.rb
+++ app/services/markdown_service.rb
@@ -6,10 +6,32 @@ require 'rouge/plugins/redcarpet'
 #
 class MarkdownService
   #
-  # enable rouge in redcarpet
+  # Create a custom renderer that sets custom classes for certain elements.
   #
   class Renderer < Redcarpet::Render::HTML
+    # enable rouge language syntax highlighter in redcarpet
     include Rouge::Plugins::Redcarpet
+
+    def link(link, title, content)
+      %(<a href="#{link}" title="#{title}" class="dark:text-primary-500">#{content}</a>)
+    end
+
+    def header(text, header_level)
+      %(<h#{header_level} class="dark:text-primary-1000">#{text}</h#{header_level}>)
+    end
+
+
+    def block_quote(quote)
+      %(<blockquote class="dark:text-primary-1000">#{quote}</blockquote>)
+    end
+
+    def double_emphasis(text)
+      %(<strong class="dark:text-primary-1000">#{text}</strong>)
+    end
+
+    def codespan(code)
+      %(<code class="dark:text-primary-1000">#{ERB::Util.h(code)}</code>)
+    end
   end
 
   attr_reader :markdown
