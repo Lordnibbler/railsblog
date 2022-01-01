b3121ab0a2a792906ee472609dd94906da385833
Ben Radler <ben@benradler.com>
2022-01-01 00:26:55 -0500

    fix for min-height: 100vh

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,6 +1,16 @@
 import fitvids from 'fitvids';
 
 $(document).on('turbo:load', function() {
+  // webkit "bug" means 100vh includes hidden area below navigation bar on iOS/iPadOS
+  // set a css variable `--appHeight` so we can use the window's innerHeight to set the page height
+  // link: https://bugs.webkit.org/show_bug.cgi?id=141832
+  // code snippet: https://stackoverflow.com/a/60657968/418864
+  const appHeight = () => {
+    document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
+  };
+  window.addEventListener('resize', appHeight);
+  appHeight();
+
   // ensure videos fit width of page
   fitvids('#main');
 
