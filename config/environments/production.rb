aaf6c31ad0de49cd68943f1c759ce92afcc04119
Ben Radler <ben@benradler.com>
2022-01-16 13:01:41 -0500

    skip assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,16 +30,16 @@ Rails.application.configure do
 
   # TODO: remove?
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # TODO: remove?
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  # config.assets.digest = true
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
--- lib/tasks/assets.rake
+++
@@ -1,7 +0,0 @@
-# https://stackoverflow.com/a/30344992/418864
-Rake::Task['assets:precompile'].clear
-  namespace :assets do
-    task 'precompile' do
-    puts 'SKIPPING rake assets:precompile'
-  end
-end
