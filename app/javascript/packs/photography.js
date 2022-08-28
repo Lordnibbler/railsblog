26110d076391b90481fdee7c3d1e848e5b131a6a
Ben Radler <ben@benradler.com>
2022-08-28 15:46:16 -0700

    DELETE THIS COMMIT: COMMENTS OUT THINGS JUST TO GET ESBUILD TO RUN

--- app/javascript/packs/photography.js
+++ app/javascript/packs/photography.js
@@ -1,5 +1,5 @@
-import 'photoswipe/dist/photoswipe.css'
-import 'photoswipe/dist/default-skin/default-skin.css'
+// import 'photoswipe/dist/photoswipe.css'
+// import 'photoswipe/dist/default-skin/default-skin.css'
 import PhotoSwipe from 'photoswipe';
 import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
 import InfiniteScroll from 'infinite-scroll';
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,10 @@ module.exports = {
         },
         extend: {
             backgroundImage: {
-                'work-bg-light': "url('~images/home/experience-figure.svg')",
-                'work-bg-dark': "url('~images/home/experience-figure-dark.svg')",
+                // 'work-bg-light': "url('~images/home/experience-figure.svg')",
+                // 'work-bg-dark': "url('~images/home/experience-figure-dark.svg')",
+                'work-bg-light': "",
+                'work-bg-dark': "",
             },
             fontFamily: {
                 header: ['Raleway', "sans-serif"],
