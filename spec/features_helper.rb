30ef4d88ac4ee34ee3868e3a3dd21846b613b16b
Ben Radler <ben@benradler.com>
2023-08-20 15:14:34 -0700

    update webdrivers

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -6,7 +6,7 @@ require 'capybara/rails'
 include Warden::Test::Helpers
 Warden.test_mode!
 
-Webdrivers::Chromedriver.required_version = "114.0.5735.90"
+Webdrivers::Chromedriver.required_version = "116.0.5845.96"
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
