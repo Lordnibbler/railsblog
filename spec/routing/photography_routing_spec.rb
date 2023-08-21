55fffbacf9e558f2a86b9358cdc2b096a2b1f5f7
Ben Radler <ben@benradler.com>
2023-08-20 22:09:13 -0700

    test fixes

--- spec/routing/photography_routing_spec.rb
+++ spec/routing/photography_routing_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe PhotographyController do
   it 'successfully renders the index template on GET photography_path', type: :request do
-    allow(FlickrService).to receive(:get_photos)
+    allow(FlickrService).to receive(:get_photos_from_cache)
 
     get photography_path
 
