276d197e58dfa983d529c4e260ce461a19efb235
Ben Radler <Lordnibbler@users.noreply.github.com>
2023-12-29 15:25:26 -0800

    upgrade rails 7.1.2 (#245)
    * upgrade rails 7.1.2
    
    * activeadmin/ransack fix for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,24 @@ class User < ApplicationRecord
          :validatable
 
   has_many :posts, dependent: :destroy, class_name: 'Blog::Post'
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[
+      avatar_url
+      biography
+      created_at
+      current_sign_in_at
+      current_sign_in_ip
+      email
+      encrypted_password
+      id
+      last_sign_in_at
+      last_sign_in_ip
+      name
+      remember_created_at
+      reset_password_sent_at
+      reset_password_token
+      sign_in_count updated_at
+    ]
+  end
 end
