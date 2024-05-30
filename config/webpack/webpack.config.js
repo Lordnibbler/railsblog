797baf94c344b31f5880d4f9d35878dd8ba79e82
Ben Radler <ben@benradler.com>
2024-05-30 13:31:21 -0700

    Add optimizations to reduce the size of webpack bundle and improve perf

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const { generateWebpackConfig, merge } = require('shakapacker');
 const baseWebpackConfig = generateWebpackConfig()
+const TerserPlugin = require('terser-webpack-plugin');
 const options = {
   // resolve: {
   //   extensions: ['.mjs', '.js', '.sass', '.scss', '.css', '.module.sass', '.module.scss', '.module.css', '.png', '.svg', '.gif', '.jpeg', '.jpg', '.json'],
@@ -13,6 +14,8 @@ const options = {
           exposes: ['$', 'jQuery']
         }
       },
+
+      // Allow loading mp4 files (squarecrusher demo)
       {
         test: /\.mp4$/,
         use: [{
@@ -23,6 +26,25 @@ const options = {
           }
         }]
       },
+
+      // // Optimize Images: Use image loaders and plugins to optimize images.
+      // {
+      //   test: /\.(png|jpe?g|gif|svg)$/i,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //     },
+      //     {
+      //       loader: 'image-webpack-loader',
+      //       options: {
+      //         mozjpeg: {
+      //           progressive: true,
+      //           quality: 65,
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
       {
         test: /\.module\.s(a|c)ss$/i,
         use: [
@@ -38,8 +60,25 @@ const options = {
           },
         ],
       },
-    ]
-  }
+    ],
+  },
+  optimization: {
+    // Tree Shaking: Ensure that your project is set up to remove unused code.
+    usedExports: true,
+
+    // Split your code into smaller chunks that can be loaded on demand.
+    splitChunks: {
+      chunks: 'all',
+    },
+
+    // Minify your JavaScript files.
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+
+  // Use Production Mode: Ensure webpack is running in production mode.
+  mode: process.env.NODE_ENV,
+
 };
 
 module.exports = merge({}, baseWebpackConfig, options)
