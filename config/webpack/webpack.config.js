760bfcd4567333ae6f52dec3a710ce9355c3db98
Ben Radler <ben@benradler.com>
2024-05-30 20:16:02 -0700

    broken

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const { generateWebpackConfig, merge } = require('shakapacker');
-const baseWebpackConfig = generateWebpackConfig()
+const baseWebpackConfig = generateWebpackConfig();
 const TerserPlugin = require('terser-webpack-plugin');
+
 const options = {
-  // resolve: {
-  //   extensions: ['.mjs', '.js', '.sass', '.scss', '.css', '.module.sass', '.module.scss', '.module.css', '.png', '.svg', '.gif', '.jpeg', '.jpg', '.json'],
+  // entry: {
+  //   application: './app/javascript/packs/application.js',
+  //   // other entry points...
+  // },
+  // output: {
+  //   path: path.resolve(__dirname, '../../public/packs'),
+  //   filename: '[name].js',
+  //   publicPath: '/packs/', // Ensure the public path is set correctly
   // },
   module: {
     rules: [
@@ -14,37 +22,39 @@ const options = {
           exposes: ['$', 'jQuery']
         }
       },
-
-      // Allow loading mp4 files (squarecrusher demo)
       {
-        test: /\.mp4$/,
-        use: [{
-          loader: "file-loader",
-          options: {
-            name: "[name].[ext]",
-            outputPath: "video"
-          }
-        }]
+        test: /\.(png|jpe?g|gif|svg|mp4)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: "[name].[ext]",
+              outputPath: (url, resourcePath, context) => {
+                if (/\.mp4$/.test(resourcePath)) {
+                  return `video/${url}`;
+                }
+                return `images/${url}`;
+              },
+              publicPath: (url, resourcePath, context) => {
+                if (/\.mp4$/.test(resourcePath)) {
+                  return `/packs/video/${url}`;
+                }
+                return `/packs/images/${url}`;
+              },
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // other options...
+            },
+          },
+        ],
       },
-
-      // // Optimize Images: Use image loaders and plugins to optimize images.
-      // {
-      //   test: /\.(png|jpe?g|gif|svg)$/i,
-      //   use: [
-      //     {
-      //       loader: 'file-loader',
-      //     },
-      //     {
-      //       loader: 'image-webpack-loader',
-      //       options: {
-      //         mozjpeg: {
-      //           progressive: true,
-      //           quality: 65,
-      //         },
-      //       },
-      //     },
-      //   ],
-      // },
       {
         test: /\.module\.s(a|c)ss$/i,
         use: [
@@ -54,7 +64,6 @@ const options = {
           {
             loader: "sass-loader",
             options: {
-              // Prefer `dart-sass`
               implementation: require("sass"),
             },
           },
@@ -63,23 +72,17 @@ const options = {
     ],
   },
   optimization: {
-    // Tree Shaking: Ensure that your project is set up to remove unused code.
     usedExports: true,
-
-    // Split your code into smaller chunks that can be loaded on demand.
     splitChunks: {
       chunks: 'all',
     },
-
-    // Minify your JavaScript files.
     minimize: true,
     minimizer: [new TerserPlugin()],
   },
-
-  // Use Production Mode: Ensure webpack is running in production mode.
-  mode: process.env.NODE_ENV,
-
+  plugins: [
+    // other plugins...
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
 
-module.exports = merge({}, baseWebpackConfig, options)
-
+module.exports = merge({}, baseWebpackConfig, options);
