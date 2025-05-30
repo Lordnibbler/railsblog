081d034633a9bb72a65de91490c1915a80c60307
Ben Radler <ben@benradler.com>
2025-05-30 10:23:22 -0700

    WORKING::: always run release tasks in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 # if [ "$RELEASE_PHASE" = "true" ]; then
-  echo "---- Release phase starting ----"
+  echo "---- Release phase starting via entrypoint.sh ----"
   bundle exec rails db:migrate && echo "db:migrate finished"
   bundle exec rails cache_warmer:flickr && echo "cache_warmer done"
   echo "---- Release phase complete ----"
-  exit 0
+  # exit 0
 # fi
 
 # otherwise fall‐through to the normal web server
